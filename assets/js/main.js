0b2afac46749c8d4b18e4672b1a1b13cb7bbc8f2
Jesús López <jeslopalo@sandbox.es>
2015-11-10 22:14:25 +0100

    Decrement scroll threshold to fire topbar effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(document).foundation();
 function animateNavbar() {
 
     var masthead = $('.masthead');
-    var scrollTopThreshold= 80;
+    var scrollTopThreshold= 60;
 
     if(masthead.length == 0) {
         setOpacity(true);

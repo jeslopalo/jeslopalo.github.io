d405fb203685197784c5ce0f32dc0a397cf99bb1
Jesús López <jeslopalo@sandbox.es>
2015-11-13 15:20:11 +0100

    Remove 'last tweet' author because was confusing

--- assets/js/twitter.js
+++ assets/js/twitter.js
@@ -18,12 +18,10 @@ function handleLastTweet(tweets) {
         var tweetUrl= $("<div>" + tweets[0] + "</div>").find("p.timePosted a").attr("href");
 
         var tweet= $("<div>" + tweets[0] + "</div>").find("p.tweet").html();
-        var user= "<div class='author'><a href='https://twitter.com/jeslopalo'>@jeslopalo</a></div>";
 
         $('#twitter-last-tweet')
             .append("<a href='" + tweetUrl + "'><time class='timePosted timeago' datetime='" + timePosted + "'>" + timePosted + "</time></a>")
-            .append("<div class='tweet'>" + tweet + "</div>")
-            .append(user);
+            .append("<div class='tweet'>" + tweet + "</div>");
 
         if(jQuery.timeago) {
             $("#twitter-last-tweet time").timeago($("#twitter-last-tweet time[datetime]").val());
